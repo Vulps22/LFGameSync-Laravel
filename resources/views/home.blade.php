d78f97a032a5b71233561f4ce78e07648242b81b
Vulps <ajmcallister5@gmail.com>
2023-08-11 00:36:06 +0100

    Replaced "getting started" button

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,8 +8,10 @@
 	<div class="max-w-3xl mx-auto text-center">
 		<h1 class="text-3xl font-bold">Meet Gamers. Make Friends.</h1>
 		<p class="text-xl mt-4">Connect your gaming profiles and find new squadmates today!</p>
-		<button class="bg-indigo-500 text-white px-6 py-3 rounded-full mt-8 hover:bg-indigo-400">Get Started</button>
-	</div>
+		<button class="bg-white text-indigo-500 px-6 py-3 rounded-full mt-8" onclick="window.location.href='/login/discord'">
+			<i class="fa-brands fa-discord"></i>
+			Sign In with Discord
+		</button>	</div>
 </header>
 
 <section id="features" class="bg-gray-800 py-20">
