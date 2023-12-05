1739290caea122ee9ad035ca5f2f36b2c777cabd
Vulps <ajmcallister5@gmail.com>
2023-12-05 00:47:12 +0000

    whereHas

--- app/Http/Controllers/LFGController.php
+++ app/Http/Controllers/LFGController.php
@@ -83,7 +83,7 @@ class LFGController extends Controller
 	public function remove_server(Request $request)
 	{
 		$server_id = $request->input('server_id');
-		$server = DiscordServer::first(['discord_id' => $server_id]);
+		$server = DiscordServer::whereHas('server_id', $server_id)->first();
 		if(!$server) return "Nothing to remove";
 var_dump($server);
 exit;
