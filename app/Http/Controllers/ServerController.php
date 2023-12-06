87c0c75471017ee0ae134165d84a8e496b68e283
Vulps <ajmcallister5@gmail.com>
2023-12-06 00:00:36 +0000

    extra checks around $state

--- app/Http/Controllers/ServerController.php
+++ app/Http/Controllers/ServerController.php
@@ -11,27 +11,30 @@ class ServerController extends Controller
     public function set_sharing(Request $request)
     {
         $server_id = $request->input('server_id');
-        if(!$server_id) return "Server ID not provided";
+        if (!$server_id) return "Server ID not provided";
 
         $user_id = $request->input('user_id');
-        if(!$user_id) return "No User ID Provided";
-        
+        if (!$user_id) return "No User ID Provided";
+
         $state = $request->input('state');
-        if(!($state === "true" || $state === "false")) return "State must be a Boolean";
+        if (!($state === "true" || $state === "false") && !is_bool($state)) return "State must be a Boolean";
         
-        if($state === "true") $state = true;
-        else $state = false;
+        //if state is not a boolean, make it a boolean
+        if (!is_bool($state)) {
+            if ($state === "true") $state = true;
+            else $state = false;
+        }
 
         $user = User::where('discord_id', $user_id)->first();
-		if (!$user) return "User not found";
+        if (!$user) return "User not found";
 
         $user->syncDiscordServers();
 
         $server = DiscordServer::where('discord_id', $server_id)->first();
-		if (!$server) return "Server not found";
+        if (!$server) return "Server not found";
 
         $discordServerUser = $user->discordServers()->where('server_id', $server->id)->first();
-		if (!$discordServerUser) return "Server User not Registered";
+        if (!$discordServerUser) return "Server User not Registered";
 
         $discordServerUser->share_library = $state;
         $discordServerUser->save();
