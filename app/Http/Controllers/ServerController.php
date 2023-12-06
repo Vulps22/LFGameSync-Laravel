1bd21f6f3a7f7a3b6f40d710e4624d5cdadde6e4
Vulps <ajmcallister5@gmail.com>
2023-12-06 16:21:39 +0000

    Pass isLinked check back to bot

--- app/Http/Controllers/ServerController.php
+++ app/Http/Controllers/ServerController.php
@@ -11,10 +11,10 @@ class ServerController extends Controller
     public function set_sharing(Request $request)
     {
         $server_id = $request->input('server_id');
-        if (!$server_id) return "Server ID not provided";
+        if (!$server_id) return json_encode(["message" => "Server ID not provided"]);
 
         $user_id = $request->input('user_id');
-        if (!$user_id) return "No User ID Provided";
+        if (!$user_id) return json_encode(["message" => "No User ID Provided"]);
 
         $state = $request->input('state');
         if (!($state === "true" || $state === "false") && !is_bool($state)) return "State must be a Boolean";
@@ -26,19 +26,21 @@ class ServerController extends Controller
         }
 
         $user = User::where('discord_id', $user_id)->first();
-        if (!$user) return "User not found";
+        if (!$user) return json_encode(["message" => "User not found"]);
 
         $user->syncDiscordServers();
 
         $server = DiscordServer::where('discord_id', $server_id)->first();
-        if (!$server) return "Server not found";
+        if (!$server) return json_encode(["message" => "Server not found"]);
 
         $discordServerUser = $user->discordServers()->where('server_id', $server->id)->first();
-        if (!$discordServerUser) return "Server User not Registered";
+        if (!$discordServerUser) return json_encode(["message" => "Server User not Registered"]);
+
+        $isLinked = $user->linkedAccounts->steam_id ? true : false;
 
         $discordServerUser->share_library = $state;
         $discordServerUser->save();
 
-        return "Sharing Set to $state";
+        return json_encode(["message" => "Sharing Changed", "state" => $state, "isLinked" => $isLinked]);
     }
 }
