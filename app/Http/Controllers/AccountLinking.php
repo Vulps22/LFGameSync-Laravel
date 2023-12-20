01ee6ec7726fb759ba34812540fc193c5efcebd2
Vulps <ajmcallister5@gmail.com>
2023-12-20 22:20:43 +0000

    dump the user

--- app/Http/Controllers/AccountLinking.php
+++ app/Http/Controllers/AccountLinking.php
@@ -7,28 +7,34 @@ use App\Models\LinkToken;
 use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\Auth;
 
-class AccountLinking extends Controller  
+class AccountLinking extends Controller
 {
 
     public $token;
 
     public function __construct()
     {
-        if(Auth::check()) return;
-
-        $token = request()->get('token'); 
-        if(!$token) { echo "Token Not Provided"; return; }
+        if (Auth::check()) return;
+        dd(Auth::user());
+        $token = request()->get('token');
+        if (!$token) {
+            echo "Token Not Provided";
+            return;
+        }
         $this->token = LinkToken::where('token', $token)->first();
         Auth::loginUsingId($this->token->user_id);
-        if(!Auth::check()) {echo "Authentication Failed, Please Try again or open a ticket on the Support Server"; exit();}
+        if (!Auth::check()) {
+            echo "Authentication Failed, Please Try again or open a ticket on the Support Server";
+            exit();
+        }
         Auth::user()->isTokenLogin = false;
     }
 
-    public function index(): View {
+    public function index(): View
+    {
 
         return view('account-linking', [
             'token' => $this->token
         ]);
     }
-
 }
