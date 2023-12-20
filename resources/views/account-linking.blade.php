9acda34c87b230f594021ebfccfa692414c94e02
Vulps <ajmcallister5@gmail.com>
2023-12-20 22:01:27 +0000

    remove useGameAccounts From Discord profile

--- resources/views/account-linking.blade.php
+++ resources/views/account-linking.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 <div>
 <div class="mx-[45%] mt-52">
-        <livewire:profile-card type="Discord" useGameAccounts="false" />
+        <livewire:profile-card type="Discord" />
     </div>
-    <div class="mx-[45%] mt-52">
+    <div class="mx-[45%] mt-20">
         <livewire:profile-card type="Steam" useGameAccounts="true" />
     </div>
 </div>
