5c0abd5f0a0aee4fa453384ae7edd13764536eee
Vulps <ajmcallister5@gmail.com>
2023-12-31 19:22:40 +0000

    more mobile friendly design

--- resources/views/account-linking.blade.php
+++ resources/views/account-linking.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
 <div>
-    <div class="mx-10 md:mx-[45%] mt-52">
+    <div class="mx-10 md:mx-52 lg:mx-[45%] mt-52">
             <livewire:profile-card type="Discord" />
         </div>
-        <div class="mx-10 md:mx-[45%] mt-20">
+        <div class="mx-10 md:mx-52 lg:mx-[45%] mt-20">
             <livewire:profile-card type="Steam" useGameAccounts="true" />
         </div>
     </div>
