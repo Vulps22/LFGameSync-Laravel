5e59e0ce6ed9b572b611892c25116d9d1e135eb6
Vulps <ajmcallister5@gmail.com>
2023-12-31 14:08:48 +0000

    extracted error page template

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,16 +2,5 @@
 
 @section('content')
 
-<div class="flex items-center justify-center h-screen">
-
-  <div class="text-center">
-
-    <h1 class="text-9xl font-bold text-gray-700">404</h1>
-
-    <p class="text-2xl font-medium text-gray-500 mt-4">Page Not Found</p>
-
-  </div>
-
-</div>
-
+<div><livewire:error-code status="404" message="Page Not Found" details="Double check the URL is correct. If you believe this is broken, open a ticket on the support server"/></div>
 @endsection
