b0b996b360fe732acefe2934a5437bd26d3e39fe
Vulps <ajmcallister5@gmail.com>
2023-12-31 13:51:12 +0000

    Now begins the effort to make error pages

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="flex items-center justify-center h-screen">
+
+  <div class="text-center">
+
+    <h1 class="text-9xl font-bold text-gray-700">404</h1>
+
+    <p class="text-2xl font-medium text-gray-500 mt-4">Page Not Found</p>
+
+  </div>
+
+</div>
+
+@endsection
