bcad519d31ee0a148469ce99bc5ac555ad80227c
Vulps <ajmcallister5@gmail.com>
2023-12-31 19:29:20 +0000

    smaller top margin

--- resources/views/account-linking.blade.php
+++ resources/views/account-linking.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div>
-    <div class="mx-10 md:mx-52 lg:mx-[45%] mt-24 md:mt-52">
+    <div class="mx-10 md:mx-52 lg:mx-[45%] mt-10 md:mt-52">
             <livewire:profile-card type="Discord" />
         </div>
         <div class="mx-10 md:mx-52 lg:mx-[45%] mt-20">
