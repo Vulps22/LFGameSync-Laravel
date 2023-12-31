7dab8286dffb87b6d3e8161d86b054309e5af333
Vulps <ajmcallister5@gmail.com>
2023-12-31 20:25:23 +0000

    use flex

--- resources/views/account-linking.blade.php
+++ resources/views/account-linking.blade.php
@@ -8,6 +8,8 @@
     <div class="mx-10 md:mx-52 lg:mx-[40%] mt-10">
         <livewire:profile-card type="Steam" useGameAccounts="true" />
     </div>
-<p class="text-md mt-10 text-centered">Remember to use /sharing on the specific servers you want to share your library on</p>
+    <div class="flex justify-center">
+        <p class="text-md mt-10 text-centered">Remember to use /sharing on the specific servers you want to share your library on</p>
+    </div>
 </div>
 @endsection
