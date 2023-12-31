ade2b03d62744429fad238a699280769c3c45f20
Vulps <ajmcallister5@gmail.com>
2023-12-31 20:23:01 +0000

    little /sharing reminder

--- resources/views/account-linking.blade.php
+++ resources/views/account-linking.blade.php
@@ -3,11 +3,11 @@
 @section('content')
 <div>
     <div class="mx-10 md:mx-52 lg:mx-[40%] mt-10 lg:mt-52">
-            <livewire:profile-card type="Discord" />
-        </div>
-        <div class="mx-10 md:mx-52 lg:mx-[40%] mt-10">
-            <livewire:profile-card type="Steam" useGameAccounts="true" />
-        </div>
+        <livewire:profile-card type="Discord" />
     </div>
-<div>
+    <div class="mx-10 md:mx-52 lg:mx-[40%] mt-10">
+        <livewire:profile-card type="Steam" useGameAccounts="true" />
+    </div>
+<p class="text-md mt-10 text-centered">Remember to use /sharing on the specific servers you want to share your library on</p>
+</div>
 @endsection
