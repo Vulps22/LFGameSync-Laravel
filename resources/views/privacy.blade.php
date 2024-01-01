123c8735f8ce9c6130ed242d8a55bb16547ebcaa
Vulps <ajmcallister5@gmail.com>
2024-01-01 01:43:04 +0000

    Updated Privacy Policy

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -25,7 +25,7 @@
 	<p class="mb-4">Data is only shared with third parties as outlined in this policy.</p>
 
 	<h2 class="text-xl font-bold mb-2">Lawful basis for processing</h2>
-	<p class="mb-4">We collect and process this data based on user consent provided when signing in with Discord.</p>
+	<p class="mb-4">We collect and process this data based on user consent provided when signing in with Discord, or while maintaining a linked Game Account</p>
 
 	<h2 class="text-xl font-bold mb-2">Data sharing</h2>
 	<p class="mb-4">We may share a user's Discord ID with Discord servers they have opted to share their library with. We also share the Discord user token with Discord as needed.</p>
@@ -35,21 +35,24 @@
 	<p class="mb-4">All data is stored on servers located in Germany.</p>
 
 	<h2 class="text-xl font-bold mb-2">Data retention</h2>
-	<p class="mb-4">We retain personal data until a user requests its removal by emailing webmaster@ajmcallister.co.uk. We will delete all data within 30 days of receiving the request.</p>
+	<p class="mb-4">We retain personal data until a user requests its removal by opening a ticket on the <a href="https://discord.gg/ZhC4JVFFUV">LFGameSync Discord Support Server</a>. We will delete all data within 30 days of receiving the request.</p>
 
 	<h2 class="text-xl font-bold mb-2">User rights</h2>
-	<p class="mb-4">Users have the right to access, correct or delete their personal data at any time. This can be done through the LFGameSync website or by emailing webmaster@ajmcallister.co.uk.</p>
+	<p class="mb-4">Users have the right to access, correct or delete their personal data at any time. This can be done through the LFGameSync website or by opening a ticket on the <a href="https://discord.gg/ZhC4JVFFUV">LFGameSync Discord Support Server</a>.</p>
 
 	<h2 class="text-xl font-bold mb-2">Cookie usage</h2>
 	<p class="mb-4">Cookies are used to store login sessions and are required for site functionality. No user tracking takes place.</p>
 
 	<h2 class="text-xl font-bold mb-2">How to opt out</h2>
-	<p class="mb-4">To opt out of all data collection and use, email webmaster@ajmcallister.co.uk requesting account deletion.</p>
+	<p class="mb-4">To opt out of all data collection and use, open a ticket on the <a href="https://discord.gg/ZhC4JVFFUV">LFGameSync Discord Support Server</a> and request account deletion.</p>
 
 	<h2 class="text-xl font-bold mb-2">Contacting us</h2>
-	<p class="mb-4">If you have any questions about this privacy policy or wish to exercise any of your rights, please contact the data controller at webmaster@ajmcallister.co.uk.</p>
+	<p class="mb-4">If you have any questions about this privacy policy or wish to exercise any of your rights, please open a ticket on the <a href="https://discord.gg/ZhC4JVFFUV">LFGameSync Discord Support Server</a></p>
+	<p class="mb-4">While we encourage all communication to take place through the Discord server as previously outlined, UK GDPR law requires we provide a means to directly contact the data controller. This can be done by DMing Vulps23 on Discord, or by emailing webmaster@ajmcallister.co.uk (Note that messages recieved via Discord will likely have a faster response time)</p>
 
 	<h2 class="text-xl font-bold mb-2">Disclaimer</h2>
 	<p>LFGameSync is not associated with Discord or Steam. References to Discord and Steam in this policy are solely for the purpose of explaining LFGameSync functionality.</p>
+
+	<sub class="text-s mb-2">Updated: 01/01/2024 at 01:42 UTC</sub>
 </div>
 @endsection
