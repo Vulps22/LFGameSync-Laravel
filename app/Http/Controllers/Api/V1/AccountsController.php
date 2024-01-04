511d9c5cd5d91e0937d62a68a62b647fddd5e526
Vulps <ajmcallister5@gmail.com>
2024-01-04 22:07:05 +0000

    Fill the field

--- app/Http/Controllers/Api/V1/AccountsController.php
+++ app/Http/Controllers/Api/V1/AccountsController.php
@@ -15,7 +15,7 @@ class AccountsController extends Controller
 
         if(!$discordId) return "Discord ID Missing";
 
-        $user = User::firstOrCreate(['discord_id' => $discordId, 'discord_name' => '']);
+        $user = User::firstOrCreate(['discord_id' => $discordId, 'discord_name' => 'Token']);
         if(!$user->exists()) $user->save();
 
         $token = hash('sha256', $discordId . now());
